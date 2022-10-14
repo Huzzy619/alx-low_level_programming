149041afdd34c8fa5e601b1417105ec2d03aeeaf
Huzzy619 <blazingkrane@gmail.com>
2022-10-13 20:27:00 -0700

    gd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+
+	return (0);
+
+
+}
