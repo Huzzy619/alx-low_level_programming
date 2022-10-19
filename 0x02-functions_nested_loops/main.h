ab826efd48f1d271a3b1fd6062f0a8e4dfe3ceb3
Huzzy619 <blazingkrane@gmail.com>
2022-10-19 21:51:53 +0100

    C functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,16 +12,5 @@
  */
 int _putchar(char c);
 
-void print_alphabet(void)
-{
-	int x;
-	for (x = 'A'; x <= 'Z'; x++)
-	{
-		char letter = tolower(x);
-		_putchar(letter);
-
-	}
-	_putchar('\n');
-	return;
-}
+void print_alphabet(void);
 #endif
