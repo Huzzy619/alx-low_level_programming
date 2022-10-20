15e695b8deb72b903ca40e4288197d09d03fbf60
Huzzy619 <blazingkrane@gmail.com>
2022-10-20 12:31:09 +0100

    C functions

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,8 +4,8 @@
   * add - add two numbers
   *@x:the first number
   *@y:the second number
-  *Return: the sum of both numbers 
-  */
+  *Return:the sum of both numbers
+ */
 
 int add(int x, int y)
 {
