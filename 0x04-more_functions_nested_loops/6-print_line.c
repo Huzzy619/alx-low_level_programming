52bdc5b32ec20fa6c1cf0a5ba3dfa8b57500d28e
Huzzy619 <blazingkrane@gmail.com>
2022-10-20 22:36:31 +0100

    C functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,17 @@
 
 void print_line(int n)
 {
+	if (n <= 0)
+		_putchar('\n');
 
-	while( n > 0 )
+	else
 	{
+		while (n > 0)
+		{
 		_putchar('_');
 		n--;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
