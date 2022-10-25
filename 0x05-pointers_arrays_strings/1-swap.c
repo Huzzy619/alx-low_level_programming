704b5c49f6cf91fec49f2dcdc103b55b226a9a79
Huzzy619 <blazingkrane@gmail.com>
2022-10-25 02:47:02 +0100

    C functions

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *swap_int - swap values
+  *@a:parameter to be swapped
+  *@b:swapped with b
+  *Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
