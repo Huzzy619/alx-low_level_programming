604d880321e704f32cb51c1bdeab28fccf903974
Huzzy619 <blazingkrane@gmail.com>
2022-10-28 03:22:28 +0100

    Pointers, arrays and Strings

--- 0x06-pointers_arrays_strings/100-strncpy.c
+++ 0x06-pointers_arrays_strings/100-strncpy.c
@@ -11,20 +11,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len;
-	int i;
 
-	len = 0;
-	while (dest[len] != '\0')
+	char *ptr = dest;
+
+	while (*src && n--)
 	{
-		++len;
-	}
-	for (i = 0; src[i] != '\0' && i < n; ++i)
-	{
-		dest[i] = src[i];
+		*dest = *src;
+		dest++;
+		src++;
 	}
 
-	dest[i] = '\0';
+	*dest = '\0';
 
-	return (dest);
+	return (ptr);
+
 }
