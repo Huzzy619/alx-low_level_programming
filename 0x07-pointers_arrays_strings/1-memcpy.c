de3c75ee669b601919524c536bec73244817c4d6
Huzzy619 <blazingkrane@gmail.com>
2022-11-01 09:10:42 +0100

    Pointers to Pointers, and 2 Dimentional arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *_memcpy - copies n bytes from src to dest
+  *@dest: dksjk
+  *@src: sds
+  *@n: dsszfd
+  *Return: A pointer to dest
+  */
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	while (n)
+	{
+		*(dest++) = *(src++);
+		--n;
+	}
+
+	return (dest);
+
+}
