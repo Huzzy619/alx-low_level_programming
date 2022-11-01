b4e093f2e8f1715e897ad474ea451015fcb64776
Huzzy619 <blazingkrane@gmail.com>
2022-11-01 09:16:26 +0100

    Pointers to Pointers, and 2 Dimentional arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int i;
 
-	while (n)
+	for (i = 0; i < n; i++)
 	{
-		*(dest++) = *(src++);
-		--n;
+		dest[i] = src[i];
 	}
 
 	return (dest);
