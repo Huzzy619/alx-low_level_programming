33ebea40601cd98774321b0985b2b7472d2bfb54
Huzzy619 <blazingkrane@gmail.com>
2022-11-05 00:22:30 +0100

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+  *main - the entry point
+  *@argc: argument count
+  *@argv: argument vector which is an array
+  *Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
