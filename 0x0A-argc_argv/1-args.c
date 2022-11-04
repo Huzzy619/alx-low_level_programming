ed58a44b551331f88e2fe6a80f0a1edd2e3a490e
Huzzy619 <blazingkrane@gmail.com>
2022-11-05 00:37:01 +0100

    argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+  *main - the entry point
+  *@argc: argument count
+  *@argv: argument vector which is an array
+  *Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+
+	return (0);
+}
+
