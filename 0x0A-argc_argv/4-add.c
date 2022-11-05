57811fa59d4ab3f99b5fd37e48e91e20342f41e5
Huzzy619 <blazingkrane@gmail.com>
2022-11-05 19:03:44 +0100

    argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+
+/**
+  *main - the entry point
+  *@argc: argument count
+  *@argv: argument vector which is an array
+  *Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+	int x;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+
+		return (1);
+	}
+
+	argc = argc - 1;
+	x = 1;
+	while (argc--)
+	{
+		if (!isdigit(*argv[x]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result = result + atoi(argv[x]);
+		x++;
+	}
+
+	printf("%d\n", result);
+
+
+	return (0);
+}
+
