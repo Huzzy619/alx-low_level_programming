ab6bebda4b083c494e67a289a5854387c28f5dfb
Huzzy619 <blazingkrane@gmail.com>
2022-11-15 04:00:53 +0100

    Structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
