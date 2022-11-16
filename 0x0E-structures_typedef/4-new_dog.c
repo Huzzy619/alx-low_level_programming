c747c9825e5016def4ef2e29b5131298e3c6ffca
Huzzy619 <blazingkrane@gmail.com>
2022-11-16 02:24:18 +0100

    Structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * _strlen - returns the lenght of a string
  * @s: pointer to s
  *
  * Return: 0 on success
  *
- */
+ 
 int _strlen(char *s)
 {
 	int count = 0;
@@ -19,7 +20,7 @@ int _strlen(char *s)
 	}
 	return (count);
 }
-/**
+**
  * _strcpy - copies the string with \0 to the buffer
  * @dest: pointer to dest
  * @src: pointer to string
@@ -55,16 +56,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	len_name = _strlen(name);
+	len_name = strlen(name);
 	new_dog->name = malloc(sizeof(char) * (len_name + 1));
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->name = strcpy(new_dog->name, name);
 	new_dog->age = age;
-	len_owner = _strlen(owner);
+	len_owner = strlen(owner);
 	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
 	if (new_dog->owner == NULL)
 	{
@@ -72,6 +73,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->owner = strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
