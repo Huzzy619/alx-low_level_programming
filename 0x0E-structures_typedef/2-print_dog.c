58578d2fffd7cd4cab8caf03b9c228c928b2dc5f
Huzzy619 <blazingkrane@gmail.com>
2022-11-16 01:41:02 +0100

    Structures and typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stddef.h>
+/**
+ *print_dog - the print function
+ *@d: the struct that is passsed
+ *Return: the instance created
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+
+	if (d->age != NULL)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+
+}
